21564dad4cd843fc6714ca941978981fdf35b64d
vishnupriya <vishnupriyayadav10@gmail.com>
2022-09-01 15:43:37 +0530

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text=System.IO.File.ReadAllText(@"E:\Projects\RainbowSchools.txt");
+            System.Console.WriteLine(text);
+            Console.ReadLine();
+
+        }
+    }
+}
